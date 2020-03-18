f37bf3c343720744e16129e7d9bdebc2b81f8ec7
Thomas Castleman <thomas7rex@gmail.com>
2020-03-18 19:04:33 -0400

    Add controller for user queries

---
+++ database/user.js
@@ -0,0 +1,38 @@
+
+/*
+  user.js: User-related database queries
+*/
+
+const con = require('../database.js').connection;
+
+module.exports = {
+  /*  addUser :: (name :: String, email :: String -> 
+    { uid :: Number, name :: String, email :: String })
+      Adds a new user to the database, returning  their profile */
+  addUser: (name, email, cb) => {
+    const handle = (err, rows) => {
+      if (err) cb(err);
+
+      if (rows.length < 2 || rows[1].length < 1) {
+        cb("Failed to retrieve user profile");
+      }
+
+      // send back the profile
+      cb(err, rows[1][0]);
+    }
+
+    con.query(
+      `INSERT INTO users (name, email) VALUES (?, ?); SELECT * FROM 
+      users WHERE uid = LAST_INSERT_ID();`,
+      [name, email], handle)
+  }
+}
+
+/*
+// Tests
+
+module.exports.addUser('Testing User', 'test@gmail.com', (err, profile) => {
+  console.log(err);
+  console.log(profile);
+});
+*/
