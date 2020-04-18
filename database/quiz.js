ff03d37d76b4cba5d56c19414127ac989910d0c6
Thomas Castleman <thomas7rex@gmail.com>
2020-04-18 11:21:38 -0400

    Add processing of quiz parameters

--- database/quiz.js
+++ database/quiz.js
@@ -8,10 +8,35 @@ const sys = require('../settings.js');
 
 module.exports = {
 
-  // constructs a state string for the quiz given the group UIDs to
-  // pull cards from
-  stateStringByGroups: (groupUIDs, cb) => {
+  /*  uidStringFromGroups :: (groupUIDs :: List<Number> -> String)
+      Construct a string representing the UID of every flashcard in
+      the union of all the groups indicated by the input list */
+  uidStringFromGroups: (groupUIDs, cb) => {
+    if (groupUIDs.length == 0) return cb(new Error("No group UIDs given."));
 
+    const makeString = (err, cards) => {
+      if (err) return cb(err);
+      const stringVersion = cards.map(c => c.flashcard_uid).join(',');
+      cb(err, stringVersion);
+    }
+
+    // get all flashcards that are in the union of the given groups
+    con.query(`
+      SELECT * FROM 
+        in_group ig JOIN flashcards f ON ig.flashcard_uid = f.uid
+      WHERE ig.group_uid IN (?)
+      GROUP BY ig.flashcard_uid;`,
+      [groupUIDs], makeString);
   }
 
 }
+
+// Tests
+/*
+
+module.exports.uidStringFromGroups([2], (err, str) => {
+  console.log(err);
+  console.log(str);
+});
+
+*/
--- routes/quiz.js
+++ routes/quiz.js
@@ -6,8 +6,24 @@
 const mid         = require('../middleware.js');
 const cardCtrl    = require('../database/flashcard.js');
 const groupCtrl   = require('../database/group.js');
+const quizCtrl    = require('../database/quiz.js');
 const Joi         = require('joi');
 const vld         = require('express-joi-validation').createValidator({});
+
+const setupSchema = Joi.object({
+  selection_style: Joi.string().valid('random','accuracy-based').required(),
+  prompting: Joi.string().valid('gloss_first', 'sign_first').required(),
+  which_to_study: Joi.string().valid('all_cards', 'by_group').required()
+}).options({
+  allowUnknown: true
+});
+
+const inProgressSchema = Joi.object({
+  selection_style: Joi.string().valid('random','accuracy-based').required(),
+  prompting: Joi.string().valid('gloss_first', 'sign_first').required(),
+  which_to_study: Joi.string().valid('all_cards', 'by_group').required(),
+  card_pool: Joi.string().allow('').optional()
+});
 
 module.exports = (app) => {
 
@@ -28,21 +44,42 @@ module.exports = (app) => {
     groupCtrl.allGroups(renderWithAllGroups);
   });
 
-  app.post('/quiz/start', mid.isAuth, (req, res) => {
-    console.log(req.body);
-
+  // parse the parameters for a quiz, send to confirmation page
+  app.post('/quiz/setup', mid.isAuth, vld.body(setupSchema), (req, res) => {
     /*
-        The state of the quiz is this:
-          My selection_style: random or accuracy-based
-          Pool: by group or all cards
-          [1, 2, 3, 4, ... all my flashcard UIDs in the pool im picking from ... ] (only if NOT all cards)
+      Here's what an example req.body looks like:
+        {
+          selection_style: 'random',
+          prompting: 'gloss_first',
+          which_to_study: 'by_group',
+          'include-group-37': 'on',
+          'include-group-6': 'on',
+          'include-group-38': 'on'
+        }
+      
+      These are the parameters:
+        selection_style: 'random' | 'accuracy-based'
+          (How are cards selected? At random, or by how accurate the user is?)
+        
+        prompting: 'gloss_first' | 'sign_first'
+          (Which "side" of the card should be shown first in this quiz?)
+        
+        which_to_study: 'all_cards' | 'by_group'
+          (Will the pool of flashcards be every card in the system? Or a subset
+            defined by the included groups?)
+        
+        'include-group-<n>'
+            (Indicates group with UID n should be included in this quiz)
     */
 
     if (req.body.which_to_study == "all_cards") {
-      console.log("all of them");
+      const params = {
+        selection_style: req.body.selection_style,
+        prompting: req.body.prompting,
+        which_to_study: req.body.which_to_study
+      };
 
-
-
+      res.rend('quiz/confirmation.html', { params });
 
     } else if (req.body.which_to_study == "by_group") {
       // need to parse which groups are being included
@@ -51,19 +88,43 @@ module.exports = (app) => {
       // filter out anything that's not related to including a group
       const onlyGroupIDs = keys.filter(k => k.includes("include-group-"));
 
+      // parse the UID integers from each string
       const END_PREFIX = 14; // index where the "include-group-" prefix ends
       const UIDsExtracted = onlyGroupIDs.map(str => parseInt(str.substring(14, str.length), 10));
 
+      // if there were no groups in the request
       if (UIDsExtracted.length == 0) return res.err({
         fr: 'No groups were chosen to be studied.'
       });
 
-      // construct state string here
+      const sendToConfirmationPage = (err, uidString) => {
+        if (err) return res.error({
+          r: err,
+          fr: 'Failed to gather UIDs of all groups included in quiz'
+        });
 
+        const params = {
+          selection_style: req.body.selection_style,
+          prompting: req.body.prompting,
+          which_to_study: req.body.which_to_study,
+          card_pool: uidString
+        };
 
+        res.rend('quiz/confirmation.html', { params });
+      }
+
+      // get the UID string for cards in this group union
+      quizCtrl.uidStringFromGroups(UIDsExtracted, sendToConfirmationPage);
     } else {
-
+      res.error({
+        fr: 'Invalid study mode: must study by group or all cards'
+      });
     }
+  });
+
+  // given quiz parameters, select a card to show the user
+  app.post('/quiz/inprogress', mid.isAuth, vld.body(inProgressSchema), (req, res) => {
+    res.send(req.body);
   });
 
 }
