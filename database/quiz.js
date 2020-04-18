27f6923e5c58141d050314436cf90ef09db0b023
Thomas Castleman <thomas7rex@gmail.com>
2020-04-18 00:43:00 -0400

    Start working on maintaining a state for quizzing by group

---
+++ database/quiz.js
@@ -0,0 +1,17 @@
+
+/*
+  database/quiz.js: Quiz-related database queries
+*/
+
+const con = require('../database.js').connection;
+const sys = require('../settings.js');
+
+module.exports = {
+
+  // constructs a state string for the quiz given the group UIDs to
+  // pull cards from
+  stateStringByGroups: (groupUIDs, cb) => {
+
+  }
+
+}
--- routes/quiz.js
+++ routes/quiz.js
@@ -30,6 +30,40 @@ module.exports = (app) => {
 
   app.post('/quiz/start', mid.isAuth, (req, res) => {
     console.log(req.body);
+
+    /*
+        The state of the quiz is this:
+          My selection_style: random or accuracy-based
+          Pool: by group or all cards
+          [1, 2, 3, 4, ... all my flashcard UIDs in the pool im picking from ... ] (only if NOT all cards)
+    */
+
+    if (req.body.which_to_study == "all_cards") {
+      console.log("all of them");
+
+
+
+
+    } else if (req.body.which_to_study == "by_group") {
+      // need to parse which groups are being included
+      const keys = Object.keys(req.body);
+
+      // filter out anything that's not related to including a group
+      const onlyGroupIDs = keys.filter(k => k.includes("include-group-"));
+
+      const END_PREFIX = 14; // index where the "include-group-" prefix ends
+      const UIDsExtracted = onlyGroupIDs.map(str => parseInt(str.substring(14, str.length), 10));
+
+      if (UIDsExtracted.length == 0) return res.err({
+        fr: 'No groups were chosen to be studied.'
+      });
+
+      // construct state string here
+
+
+    } else {
+
+    }
   });
 
 }
